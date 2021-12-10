9e45e3d929a92a8926db74076d246ca37afed0b7
prafpawar11@gmail.com <prafpawar11@gmail.com>
2021-12-10 20:09:00 +0530

    Second Commit

---
+++ src/main/java/BaseLayer/BaseClass.java
@@ -0,0 +1,10 @@
+package BaseLayer;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Initilizatin method in base class");
+	}
+
+}
---
+++ src/main/java/PageLayer/LoginPage.java
@@ -0,0 +1,10 @@
+package PageLayer;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Login Page in Page Layer Class");
+	}
+
+}
